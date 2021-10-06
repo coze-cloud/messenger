7f16250ead14ad89947c9068362c56e294a2f3bc
Samuel Oechsler <samuel@oechsler.it>
2021-10-06 09:57:45 +0200

    Add queue structure

---
+++ queue_test.go
@@ -0,0 +1,23 @@
+package messenger
+
+import "testing"
+
+func TestNewQueue(t *testing.T) {
+
+}
+
+func TestQueue_Named(t *testing.T) {
+
+}
+
+func TestQueue_WithTopic(t *testing.T) {
+
+}
+
+func TestQueue_ShouldAutoRemove(t *testing.T) {
+
+}
+
+func TestQueue_WithTimeToLive(t *testing.T) {
+
+}
